6e320aacafe50dad7dafc4507b5359aa4e4ff365
Amit <aweinsto@redhat.com>
2025-11-07 13:14:53 +0200

    Delete tests directory

--- tests/test_analysis.py
+++
@@ -1,4 +0,0 @@
-﻿def test_import_analysis_module():
-    import importlib
-    mod = importlib.import_module('my_tool.analysis')
-    assert hasattr(mod, 'run_analysis')
--- tests/test_data.py
+++
@@ -1,5 +0,0 @@
-﻿def test_import_data_module():
-    import importlib
-    mod = importlib.import_module('my_tool.data')
-    assert hasattr(mod, 'create_params')
-    assert hasattr(mod, 'generate_event_data')
